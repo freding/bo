fb00c8787755dfc2d01c12849fe5df4536954fff
frederic <frederic.bourbigot@openjet.com>
2017-07-02 22:18:03 +0200

    Add Api tests

--- tests/AppBundle/ApiController/ProductsControllerTest.php
+++ tests/AppBundle/ApiController/ProductsControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\AppBundle\ApiController;
 
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+
 /**
 * ProductsControllerTest
 * @author F.Bourbigot
@@ -18,8 +19,11 @@ class ProductsControllerTest extends WebTestCase
         );
         $this->loadFixtures($fixtures);
     }
-    
-    public function testGetProductAction()
+
+    /**
+     * {@ignore}
+     */    
+    public function testGetProductsAction()
     {
         $client = $this->createClient();
         
@@ -31,4 +35,26 @@ class ProductsControllerTest extends WebTestCase
         $responseContentAsArray = json_decode($responseContent);
         $this->assertEquals(count($responseContentAsArray), 3);
     }
+    
+    /**
+     * {@ignore}
+     */    
+    public function testGetProductAction()
+    {
+        $client = $this->createClient();
+        
+        $client->request('GET', '/products/1');
+        $this->assertStatusCode(Response::HTTP_OK, $client);
+        
+        /** @var string $response_content */
+        $responseContent = $this->fetchContent('/products/1', 'GET');
+        $responseContentAsArray = json_decode($responseContent);
+        $this->assertEquals(count($responseContentAsArray), 1);
+        
+        $client->request('GET', '/products/10');
+        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $client);
+        
+    }
+    
+    
 }
