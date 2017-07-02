10fd7c51abf609853ba87c67f7ef96099c48d25a
frederic <frederic.bourbigot@openjet.com>
2017-07-02 20:42:38 +0200

    Add Phpunit in Docker

--- tests/AppBundle/ApiController/ProductsControllerTest.php
+++ tests/AppBundle/ApiController/ProductsControllerTest.php
@@ -22,7 +22,13 @@ class ProductsControllerTest extends WebTestCase
     public function testGetProductAction()
     {
         $client = $this->createClient();
+        
         $client->request('GET', '/products');
         $this->assertStatusCode(Response::HTTP_OK, $client);
+        
+        /** @var string $response_content */
+        $responseContent = $this->fetchContent('/products', 'GET');
+        $responseContentAsArray = json_decode($responseContent);
+        $this->assertEquals(count($responseContentAsArray), 3);
     }
 }
