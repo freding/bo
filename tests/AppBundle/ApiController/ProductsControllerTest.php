a0b838ea6c33771466b19116e72c910510557d3c
frederic <frederic.bourbigot@openjet.com>
2017-07-03 15:52:35 +0200

    Add Api tests - product PUT/DELETE

--- tests/AppBundle/ApiController/ProductsControllerTest.php
+++ tests/AppBundle/ApiController/ProductsControllerTest.php
@@ -6,6 +6,8 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\DataFixtures\SampleData\Products;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Client;
+
 
 /**
 * ProductsControllerTest
@@ -13,6 +15,11 @@ use Symfony\Component\HttpFoundation\Request;
 */
 class ProductsControllerTest extends WebTestCase
 {
+    /**
+     * @var Client 
+     */
+    private $client;
+    
     public function setUp()
     {
         parent::setup();
@@ -20,6 +27,8 @@ class ProductsControllerTest extends WebTestCase
           'AppBundle\DataFixtures\ORM\LoadProductData',
         );
         $this->loadFixtures($fixtures);
+        
+        $this->client = $this->createClient();
     }
 
     /**
@@ -27,10 +36,8 @@ class ProductsControllerTest extends WebTestCase
      */    
     public function testGetProductsAction()
     {
-        $client = $this->createClient();
-        
-        $client->request(Request::METHOD_GET, '/products');
-        $this->assertStatusCode(Response::HTTP_OK, $client);
+        $this->client->request(Request::METHOD_GET, '/products');
+        $this->assertStatusCode(Response::HTTP_OK, $this->client);
         
         /** @var string $response_content */
         $responseContent = $this->fetchContent('/products', Request::METHOD_GET);
@@ -43,18 +50,16 @@ class ProductsControllerTest extends WebTestCase
      */    
     public function testGetProductAction()
     {
-        $client = $this->createClient();
-        
-        $client->request(Request::METHOD_GET, '/products/1');
-        $this->assertStatusCode(Response::HTTP_OK, $client);
+        $this->client->request(Request::METHOD_GET, '/products/1');
+        $this->assertStatusCode(Response::HTTP_OK, $this->client);
         
         /** @var string $response_content */
         $responseContent = $this->fetchContent('/products/1', Request::METHOD_GET);
         $responseContentAsArray = json_decode($responseContent);
         $this->assertEquals(count($responseContentAsArray), 1);
         
-        $client->request(Request::METHOD_GET, '/products/10');
-        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $client);
+        $this->client->request(Request::METHOD_GET, '/products/10');
+        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $this->client);
     }
     
     /**
@@ -63,13 +68,12 @@ class ProductsControllerTest extends WebTestCase
     public function testPostProductAction()
     {
         $server = ['CONTENT_TYPE' => 'application/json'];        
-        $client = $this->createClient();
-        
+
         $sampleDataValid = Products::getValidOne();
         $jsonSampleDataValid = json_encode($sampleDataValid);
-        $client->request(Request::METHOD_POST, "/products", array(), array(), $server, $jsonSampleDataValid);
-        $response = $client->getResponse();
-        $this->assertStatusCode(Response::HTTP_CREATED, $client);
+        $this->client->request(Request::METHOD_POST, "/products", array(), array(), $server, $jsonSampleDataValid);
+        $response = $this->client->getResponse();
+        $this->assertStatusCode(Response::HTTP_CREATED, $this->client);
 
         $sampleDataInResponse = json_decode($response->getContent());
         $this->assertEquals($sampleDataInResponse->name,$sampleDataValid["name"]);
@@ -77,8 +81,37 @@ class ProductsControllerTest extends WebTestCase
         $this->assertEquals($sampleDataInResponse->price,$sampleDataValid["price"]);
         
         $jsonSampleDataUnvalid = json_encode(Products::getUnvalidOne());
-        $client->request(Request::METHOD_POST, "/products", array(), array(), $server, $jsonSampleDataUnvalid);
-        $this->assertStatusCode(Response::HTTP_BAD_REQUEST, $client);       
+        $this->client->request(Request::METHOD_POST, "/products", array(), array(), $server, $jsonSampleDataUnvalid);
+        $this->assertStatusCode(Response::HTTP_BAD_REQUEST, $this->client);       
     }
+ 
+    /**
+     * {@ignore}
+     */
+    public function testPutProductAction()
+    {
+        $server = ['CONTENT_TYPE' => 'application/json'];        
+
+        $sampleDataUpdateable = Products::get()[0];
+        $jsonSampleDataUpdateable = json_encode($sampleDataUpdateable);
+        $this->client->request(Request::METHOD_PUT, "/products/1", array(), array(), $server, $jsonSampleDataUpdateable);
+        $this->assertStatusCode(Response::HTTP_ACCEPTED, $this->client);
+        
+        $this->client->request(Request::METHOD_PUT, "/products/100", array(), array(), $server, $jsonSampleDataUpdateable);
+        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $this->client);
+        
+    }
+    
+    /**
+     * {@ignore}
+     */
+    public function testRemovePlaceAction()
+    {
+        $this->client->request(Request::METHOD_DELETE, '/products/1');
+        $this->assertStatusCode(Response::HTTP_NO_CONTENT, $this->client);
+        
+        $this->client->request(Request::METHOD_DELETE, '/products/100');
+        $this->assertStatusCode(Response::HTTP_NOT_FOUND, $this->client);  
+    }        
     
 }
